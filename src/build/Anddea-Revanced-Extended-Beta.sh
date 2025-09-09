599be87cd498c143665c4d53446847eb6eb99c0a
NV <125726630+nidhinvijay@users.noreply.github.com>
2025-09-10 03:22:34 +0530

    Update Anddea-Revanced-Extended-Beta.sh

--- src/build/Anddea-Revanced-Extended-Beta.sh
+++ src/build/Anddea-Revanced-Extended-Beta.sh
@@ -6,13 +6,13 @@ dl_gh "revanced-patches" "anddea" "prerelease"
 dl_gh "revanced-cli" "inotia00" "latest"
 
 # # Patch YouTube:
-get_patches_key "youtube-rve-anddea"
-get_apk "com.google.android.youtube" "youtube-beta" "youtube" "google-inc/youtube/youtube" "20.34.40"
-split_editor "youtube-beta" "youtube-beta"
-patch "youtube-beta" "anddea" "inotia"
+#get_patches_key "youtube-rve-anddea"
+#get_apk "com.google.android.youtube" "youtube-beta" "youtube" "google-inc/youtube/youtube" "20.34.40"
+#split_editor "youtube-beta" "youtube-beta"
+#patch "youtube-beta" "anddea" "inotia"
 # Patch Youtube Arm64-v8a
 get_patches_key "youtube-rve-anddea"
-get_apk "com.google.android.youtube" "youtube-beta" "youtube" "google-inc/youtube/youtube" "20.34.40"
+get_apk "com.google.android.youtube" "youtube-beta" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
 split_editor "youtube-beta" "youtube-beta-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
 patch "youtube-beta-arm64-v8a" "anddea" "inotia"
 # # Patch Youtube Armeabi-v7a
