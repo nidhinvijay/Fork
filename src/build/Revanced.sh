d253e26c0ac6b69dd8b4c3bc741645fcbef44744
NV <125726630+nidhinvijay@users.noreply.github.com>
2025-09-10 00:31:42 +0530

    Update Revanced.sh

--- src/build/Revanced.sh
+++ src/build/Revanced.sh
@@ -14,6 +14,7 @@ revanced_dl(){
 	# patch "youtube" "revanced"
 	# # Patch Youtube Arm64-v8a
 	get_patches_key "youtube-revanced" 
+ 	get_apk "com.google.android.youtube" "youtube" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
 	split_editor "youtube" "youtube-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
 	patch "youtube-arm64-v8a" "revanced"
 }
