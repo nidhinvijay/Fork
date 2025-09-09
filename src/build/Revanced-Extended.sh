5cbbe5f51cd4a940946ee1d45ed365b60840771a
NV <125726630+nidhinvijay@users.noreply.github.com>
2025-09-10 00:36:40 +0530

    Update Revanced-Extended.sh

--- src/build/Revanced-Extended.sh
+++ src/build/Revanced-Extended.sh
@@ -7,6 +7,7 @@ dl_gh "revanced-patches revanced-cli" "inotia00" "latest"
 
 # Patch Youtube Arm64-v8a
 get_patches_key "youtube-revanced-extended"
+get_apk "com.google.android.youtube" "youtube" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
 split_editor "youtube" "youtube-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
 patch "youtube-arm64-v8a" "revanced-extended" "inotia"
 # # Patch Youtube Armeabi-v7a
