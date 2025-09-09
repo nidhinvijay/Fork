6e0a6a9ad35f476d24cf38a644b3e1e919f40c74
NV <125726630+nidhinvijay@users.noreply.github.com>
2025-09-10 00:08:24 +0530

    Update Revanced.sh

--- src/build/Revanced.sh
+++ src/build/Revanced.sh
@@ -7,12 +7,12 @@ revanced_dl(){
 }
 1() {
 	revanced_dl
-	# Patch YouTube:
-	get_patches_key "youtube-revanced"
-	get_apk "com.google.android.youtube" "youtube" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
-	split_editor "youtube" "youtube"
-	patch "youtube" "revanced"
-	# Patch Youtube Arm64-v8a
+	# # Patch YouTube:
+	# get_patches_key "youtube-revanced"
+	# get_apk "com.google.android.youtube" "youtube" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
+	# split_editor "youtube" "youtube"
+	# patch "youtube" "revanced"
+	# # Patch Youtube Arm64-v8a
 	get_patches_key "youtube-revanced" 
 	split_editor "youtube" "youtube-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
 	patch "youtube-arm64-v8a" "revanced"
@@ -36,22 +36,13 @@ revanced_dl(){
 }
 4() {
 	revanced_dl
-	get_apk "com.google.android.youtube" "youtube-lite" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
-	# Patch YouTube Lite Arm64-v8a:
-	get_patches_key "youtube-revanced"
-	split_editor "youtube-lite" "youtube-lite-arm64-v8a" "include" "split_config.arm64_v8a split_config.en split_config.xxxhdpi"
-	patch "youtube-lite-arm64-v8a" "revanced"
-	
-}
-5() {
-	revanced_dl
 	# Patch YouTube Music:
 	# Arm64-v8a
 	get_patches_key "youtube-music-revanced"
 	get_apk "com.google.android.apps.youtube.music" "youtube-music-arm64-v8a" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
 	patch "youtube-music-arm64-v8a" "revanced"
 }
-6() {
+5() {
 	revanced_dl
 	# Patch Spotjfy Arm64-v8a
 	get_patches_key "Spotjfy-revanced"
@@ -75,9 +66,6 @@ case "$1" in
         ;;
     5)
         5
-        ;;
-    6)
-        6
         ;;
     
 esac
